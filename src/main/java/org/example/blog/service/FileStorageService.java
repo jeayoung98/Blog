7240fc07ca0adea11dbc5ad2d3524e18c7f39418
jeayoung <jjy0843@gmail.com>
2024-06-21 20:06:44 +0900

    image

---
+++ src/main/java/org/example/blog/service/FileStorageService.java
@@ -0,0 +1,38 @@
+package org.example.blog.service;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.example.blog.domain.Image;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation = Paths.get("uploads").toAbsolutePath().normalize();
+
+    public FileStorageService() {
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("파일 저장 디렉토리를 생성할 수 없습니다.", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+        Path targetLocation = this.fileStorageLocation.resolve(fileName);
+        Files.copy(file.getInputStream(), targetLocation);
+        return targetLocation.toString();
+    }
+}
