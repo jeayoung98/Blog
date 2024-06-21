157467035bfe8216de7003c762fce8e85f134412
jeayoung <jjy0843@gmail.com>
2024-06-21 20:04:01 +0900

    update PostRepository

--- src/main/java/org/example/blog/repository/PostRepository.java
+++ src/main/java/org/example/blog/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package org.example.blog.repository;
 
+import org.example.blog.domain.Image;
 import org.example.blog.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
