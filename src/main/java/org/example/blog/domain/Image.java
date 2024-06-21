faca97605a4c7d89e26c63ac99b38424aa8735d1
jeayoung <jjy0843@gmail.com>
2024-06-21 20:20:33 +0900

    image

---
+++ src/main/java/org/example/blog/domain/Image.java
@@ -0,0 +1,24 @@
+package org.example.blog.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "images")
+@Getter
+@Setter
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+}
