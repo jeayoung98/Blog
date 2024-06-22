06234df63f91d188f69a896533d0acc38fa3c72c
jeayoung <jjy0843@gmail.com>
2024-06-22 22:41:43 +0900

    update BlogService

--- src/main/java/org/example/blog/service/BlogService.java
+++ src/main/java/org/example/blog/service/BlogService.java
@@ -1,17 +1,12 @@
 package org.example.blog.service;
 
 import org.example.blog.domain.Blog;
-import org.example.blog.domain.Image;
-import org.example.blog.domain.Post;
 import org.example.blog.domain.User;
 import org.example.blog.repository.BlogRepository;
-import org.example.blog.repository.PostRepository;
 import org.example.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class BlogService {
@@ -26,6 +21,7 @@ public class BlogService {
     public Long createBlog(String title, Long userId) {
         User user = userRepository.findById(userId).orElse(null);
         if (user == null) {
+            System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
             throw new IllegalStateException("사용자를 찾을 수 없습니다.");
         }
 
@@ -36,7 +32,7 @@ public class BlogService {
         return blog.getBlogId();
     }
 
-    public Blog getBlogById(Long id) {
+    public Blog getBlogByUserId(Long id) {
         return blogRepository.findByUserId(id);
     }
 }
