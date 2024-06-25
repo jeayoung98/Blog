ddcb971a96230b34a00be4aae7b9acce9d3e4aa9
Jaeyoung <jjy0843@gmail.com>
2024-06-25 15:13:14 +0900

    update imageUploadLocation

--- src/main/java/org/example/blog/service/FileStorageService.java
+++ src/main/java/org/example/blog/service/FileStorageService.java
@@ -1,38 +1,30 @@
 package org.example.blog.service;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.example.blog.domain.Image;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
+import java.nio.file.StandardCopyOption;
 
 
 @Service
 public class FileStorageService {
-
-    private final Path fileStorageLocation = Paths.get("uploads").toAbsolutePath().normalize();
-
-    public FileStorageService() {
-        try {
-            Files.createDirectories(this.fileStorageLocation);
-        } catch (Exception e) {
-            throw new RuntimeException("파일 저장 디렉토리를 생성할 수 없습니다.", e);
-        }
-    }
+    private final String uploadDir = "C://Temp/upload/";
 
     public String storeFile(MultipartFile file) throws IOException {
-        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-        Path targetLocation = this.fileStorageLocation.resolve(fileName);
-        Files.copy(file.getInputStream(), targetLocation);
-        return targetLocation.toString();
+        // 파일 저장 디렉토리 경로 설정
+        Path uploadPath = Paths.get(uploadDir);
+
+        // 저장할 파일의 경로 설정
+        String fileName = file.getOriginalFilename();
+        Path filePath = uploadPath.resolve(fileName);
+
+        // 파일 저장
+        Files.copy(file.getInputStream(), filePath);
+        return fileName;
     }
 }
