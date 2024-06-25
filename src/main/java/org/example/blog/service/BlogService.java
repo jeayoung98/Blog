a071b67c21492332e9bb0c475a492cedf15cca4b
Jaeyoung <jjy0843@gmail.com>
2024-06-25 15:19:43 +0900

    create sortedPost

--- src/main/java/org/example/blog/service/BlogService.java
+++ src/main/java/org/example/blog/service/BlogService.java
@@ -1,12 +1,16 @@
 package org.example.blog.service;
 
 import org.example.blog.domain.Blog;
+import org.example.blog.domain.Post;
 import org.example.blog.domain.User;
 import org.example.blog.repository.BlogRepository;
 import org.example.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlogService {
@@ -21,7 +25,6 @@ public class BlogService {
     public Long createBlog(String title, Long userId) {
         User user = userRepository.findById(userId).orElse(null);
         if (user == null) {
-            System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
             throw new IllegalStateException("사용자를 찾을 수 없습니다.");
         }
 
@@ -35,4 +38,13 @@ public class BlogService {
     public Blog getBlogByUserId(Long id) {
         return blogRepository.findByUserId(id);
     }
+
+    public void sortedPosts(Long blogId) {
+        Blog blog = blogRepository.findById(blogId).orElse(null);
+        List<Post> list = blog.getPosts().stream()
+                .sorted((post1, post2) -> post2.getTime().compareTo(post1.getTime()))
+                .toList();
+        blog.setPosts(list);
+    }
+
 }
