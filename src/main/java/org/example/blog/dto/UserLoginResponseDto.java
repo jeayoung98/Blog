3afc81822d0ac2434b5f381006257bc4baf4843b
Jaeyoung <jjy0843@gmail.com>
2024-07-08 10:42:25 +0900

    dto

---
+++ src/main/java/org/example/blog/dto/UserLoginResponseDto.java
@@ -0,0 +1,18 @@
+package org.example.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+    private Long userId;
+    private String name;
+    private Long blogId;
+}
